949285be538100300a1a05a57397a508bd248ed2
sunshi <pasquier.sosten@gmail.com>
2025-08-20 19:36:54 +0200

    adding basic features

--- lua/helix/plugins/auto_join_default_class/sh_plugin.lua
+++ lua/helix/plugins/auto_join_default_class/sh_plugin.lua
@@ -1,55 +1,100 @@
---[[
- ________  ___  ___  ________   ________  ___  ___  ___     
-|\   ____\|\  \|\  \|\   ___  \|\   ____\|\  \|\  \|\  \    
-\ \  \___|\ \  \\\  \ \  \\ \  \ \  \___|\ \  \\\  \ \  \   
- \ \_____  \ \  \\\  \ \  \\ \  \ \_____  \ \   __  \ \  \  
-  \|____|\  \ \  \\\  \ \  \\ \  \|____|\  \ \  \ \  \ \  \ 
-    ____\_\  \ \_______\ \__\\ \__\____\_\  \ \__\ \__\ \__\
-   |\_________\|_______|\|__| \|__|\_________\|__|\|__|\|__|
-   \|_________|                   \|_________|              
-]]--
-
 local PLUGIN = PLUGIN
 
 PLUGIN.name = "Auto join default class"
 PLUGIN.author = "Sunshi"
-PLUGIN.description = "Automatically join the default class when a player join a faction."
+PLUGIN.description = "Automatically join the default class when a player joins a faction."
+
+ix.config.Add("setFactionModel", true, "Set automatically faction or class model.", nil, {
+    category = "Characters"
+})
 
 if SERVER then
-local character = ix.meta.character
-local _oldSetFaction = character.SetFaction
+    local character = ix.meta.character
+    local _oldSetFaction = character.SetFaction
 
-function character:SetFaction(faction, ...)
-    _oldSetFaction(self, faction, ...)
-    hook.Run("OnCharacterTransferred", self, faction)
-end
-
-hook.Add("OnCharacterTransferred", "SetDefaultClass", function(character, faction)
-    local client = character:GetPlayer()
-    if isstring(ix.faction.Get(faction).models) then
-     character:SetModel(ix.faction.Get(faction).models)
-    elseif istable(ix.faction.Get(faction).models) then
-     character:SetModel(ix.faction.Get(faction).models[math.random(1, table.Count(ix.faction.Get(faction).models))])
+    function character:SetFaction(faction, ...)
+        _oldSetFaction(self, faction, ...)
+        hook.Run("OnCharacterTransferred", self, faction)
     end
 
-    for i,v in ipairs(ix.class.list) do
-        if v.faction == faction then
-           if v.isDefault then
-              character:JoinClass(i)
-              break
-           end
+    hook.Add("OnCharacterTransferred", "SetDefaultClass", function(character, faction)
+        character:SetDefaultClass(faction)
+        if ix.config.Get("setFactionModel") then
+        character:SetFactionModel(faction)
+        local class = GetDefaultClass(faction)
+        if class then
+            character:SetClassModel(class)
+        end
+        end
+    end)
+
+    function PLUGIN:PlayerLoadedCharacter(client, character, currentChar)
+    end
+
+    function PLUGIN:OnCharacterDisconnect(client, character)
+        if ix.config.Get("setFactionModel") then
+        local faction = character:GetFaction()
+        character:SetClassModel(GetDefaultClass(faction))
         end
     end
-end)
 
-
-function PLUGIN:PlayerJoinedClass(client, class, oldClass)
-                local character = client:GetCharacter()
-    if isstring(ix.class.Get(class).model) then
-     character:SetModel(ix.class.Get(class).model)
-     if ix.class.Get(class).skin then client:SetSkin(ix.class.Get(class).skin) else client:SetSkin(0) end
+    function PLUGIN:PlayerJoinedClass(client, class, oldClass)
+        if ix.config.Get("setFactionModel") then
+        local character = client:GetCharacter()
+        character:SetClassModel(class)
+        end
     end
-end
 
+    function PLUGIN:CharacterLoaded(character)
+        local client = character:GetPlayer()
+        local faction = character:GetFaction()
+        local defaultClass = GetDefaultClass(faction)
+        local class = character:GetClass()
+        if ix.config.Get("setFactionModel") then
+        character:SetFactionModel(faction)
+        if defaultClass then
+            character:SetClassModel(defaultClass)
+        end
+        end
+    end
 
+    function GetDefaultClass(faction)
+        for i, v in ipairs(ix.class.list) do
+            if v.faction == faction and v.isDefault then
+                return i
+            end
+        end
+        return nil
+    end
+
+    function character:SetDefaultClass(faction)
+        for i, v in ipairs(ix.class.list) do
+            if v.faction == faction and v.isDefault then
+                self:JoinClass(i)
+                break
+            end
+        end
+    end
+
+    function character:SetClassModel(class)
+        local client = self:GetPlayer()
+        if not class then return end
+        local data = ix.class.Get(class)
+        if not data then return end
+        if isstring(data.model) then
+            self:SetModel(data.model)
+            client:SetSkin(data.skin or 0)
+        end
+    end
+
+    function character:SetFactionModel(faction)
+        local client = self:GetPlayer()
+        local data = ix.faction.Get(faction)
+        if not data then return end
+        if isstring(data.models) then
+            self:SetModel(data.models)
+        elseif istable(data.models) then
+            self:SetModel(data.models[math.random(1, table.Count(data.models))])
+        end
+    end
 end
